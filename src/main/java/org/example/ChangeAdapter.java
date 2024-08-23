de4b656d6e5b2877e985a43060ed20e2b42ff8f4
Gleb Tkach <71315071+Gleb647@users.noreply.github.com>
2024-08-23 16:02:37 +0300

    Delete src/main/java/org/example/ChangeAdapter.java

--- src/main/java/org/example/ChangeAdapter.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-import org.openstreetmap.josm.data.osm.event.DataChangedEvent;
-
-public interface ChangeAdapter {
-    //void notifyDataChanged(AbstractDataChangedEvent event);
-}
